38ccb7e62f6c214d3a7bf1e0b77ee251078025a3
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 16:05:11 -0500

    Update app.py

--- streamlit_led_control/app.py
+++ streamlit_led_control/app.py
@@ -1,53 +1,58 @@
+import paho.mqtt.client as paho
+import time
+import json
 import streamlit as st
-import paho.mqtt.client as mqtt
+import cv2
+import numpy as np
+from PIL import Image, ImageOps
+from keras.models import load_model
 
-# Configuración del broker MQTT
-mqtt_broker = "broker.mqttdashboard.com"
-mqtt_port = 1883
-mqtt_topic = "Palmadas"
-
-# Callback de conexión al broker MQTT
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        st.write("Conectado al broker MQTT.")
-    else:
-        st.write(f"Falla en conexión, código de retorno: {rc}")
+# Callback de publicación
+def on_publish(client, userdata, result):
+    print("El dato ha sido publicado\n")
+    pass
 
 # Callback de mensaje
-def on_message(client, userdata, msg):
-    st.write(f"Mensaje recibido: {msg.topic} {msg.payload.decode()}")
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received = str(message.payload.decode("utf-8"))
+    st.write(message_received)
 
-# Inicializa cliente MQTT
-def initialize_mqtt():
-    try:
-        client_id = "control_led_app"
-        client = mqtt.Client(client_id)
-        client.on_connect = on_connect
-        client.on_message = on_message
+# Configuración del broker MQTT
+broker = "broker.mqttdashboard.com"
+port = 1883
+client1 = paho.Client("APP_CERR")
+client1.on_message = on_message
+client1.on_publish = on_publish
+client1.connect(broker, port)
+client1.loop_start()  # Iniciar el bucle de la red en segundo plano
 
-        client.connect(mqtt_broker, mqtt_port, 60)
-        client.loop_start()  # Iniciar el bucle de la red en segundo plano
-        return client
-    except Exception as e:
-        st.write(f"Error al inicializar el cliente MQTT: {e}")
-        return None
-
-client = initialize_mqtt()
-
-# Función para enviar mensaje de aplauso
-def send_clap():
-    if client:
-        try:
-            client.publish(mqtt_topic, "Palmada")
-            st.write("Mensaje de aplauso enviado.")
-        except Exception as e:
-            st.write(f"Error al enviar el mensaje: {e}")
-    else:
-        st.write("Cliente MQTT no está inicializado.")
+# Cargar el modelo de Keras
+model = load_model('keras_model.h5')
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Interfaz de usuario de Streamlit
-st.title("Control de LED con Aplausos")
-st.write("Presiona el botón para simular un aplauso y controlar el LED.")
+st.title("Cerradura Inteligente")
 
-if st.button("Simular Aplauso"):
-    send_clap()
+img_file_buffer = st.camera_input("Toma una Foto")
+
+if img_file_buffer is not None:
+    img = Image.open(img_file_buffer)
+    img = img.resize((224, 224))
+    img_array = np.array(img)
+    
+    # Normalizar la imagen
+    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    # Realizar la inferencia
+    prediction = model.predict(data)
+    if prediction[0][0] > 0.3:
+        st.header('Abriendo')
+        client1.publish("IMIA", json.dumps({"gesto": "Abre"}), qos=0, retain=False)
+        time.sleep(0.2)
+    elif prediction[0][1] > 0.3:
+        st.header('Cerrando')
+        client1.publish("IMIA", json.dumps({"gesto": "Cierra"}), qos=0, retain=False)
+        time.sleep(0.2)
