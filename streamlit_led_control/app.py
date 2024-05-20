a2c221498ff551d9721bb6cbc556d1038e95ad67
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 15:44:14 -0500

    Update app.py

--- streamlit_led_control/app.py
+++ streamlit_led_control/app.py
@@ -13,31 +13,25 @@ def on_connect(client, userdata, flags, rc):
     else:
         st.write(f"Falla en conexión, código de retorno: {rc}")
 
-# Callback para manejar mensajes
-def on_message(client, userdata, message):
-    st.write(f"Mensaje recibido: {message.payload.decode()}")
+# Inicializa cliente MQTT
+client_id = "control_led_app"
+client = mqtt.Client(client_id)
+client.on_connect = on_connect
 
-client = None
+# Intentar conectar al broker MQTT
 try:
-    # Inicializa cliente MQTT con la versión de la API
-    client_id = "control_led_app"
-    client = mqtt.Client(client_id, protocol=mqtt.MQTTv311)
-    client.on_connect = on_connect
-    client.on_message = on_message
     client.connect(mqtt_broker, mqtt_port, 60)
-    client.loop_start()
-except ValueError as e:
-    st.write(f"Error al crear el cliente MQTT: {e}")
+    client.loop_start()  # Iniciar el bucle de la red en segundo plano
 except Exception as e:
-    st.write(f"Otro error ocurrió: {e}")
+    st.write(f"Error al conectar al broker MQTT: {e}")
 
 # Función para enviar mensaje de aplauso
 def send_clap():
-    if client:
-        client.publish(mqtt_topic, "Palmada")
+    if client.is_connected():
+        client.publish(mqtt_topic, "palmada")
         st.write("Mensaje de aplauso enviado.")
     else:
-        st.write("Cliente MQTT no está inicializado.")
+        st.write("Cliente MQTT no está conectado.")
 
 # Interfaz de usuario de Streamlit
 st.title("Control de LED con Aplausos")
