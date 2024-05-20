84c3f056a7bff681ac4cb49ec8878b9752b62e8f
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 16:10:41 -0500

    Update app.py

--- streamlit_led_control/app.py
+++ streamlit_led_control/app.py
@@ -2,20 +2,18 @@ import paho.mqtt.client as paho
 import time
 import json
 import streamlit as st
-import cv2
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import Image
 from keras.models import load_model
 
 # Callback de publicación
 def on_publish(client, userdata, result):
-    print("El dato ha sido publicado\n")
+    st.write("El dato ha sido publicado")
     pass
 
 # Callback de mensaje
 def on_message(client, userdata, message):
     global message_received
-    time.sleep(2)
     message_received = str(message.payload.decode("utf-8"))
     st.write(message_received)
 
@@ -43,21 +41,24 @@ st.title("Cerradura Inteligente")
 img_file_buffer = st.camera_input("Toma una Foto")
 
 if img_file_buffer is not None:
-    img = Image.open(img_file_buffer)
-    img = img.resize((224, 224))
-    img_array = np.array(img)
-    
-    # Normalizar la imagen
-    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
-    data[0] = normalized_image_array
+    try:
+        img = Image.open(img_file_buffer)
+        img = img.resize((224, 224))
+        img_array = np.array(img)
+        
+        # Normalizar la imagen
+        normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
+        data[0] = normalized_image_array
 
-    # Realizar la inferencia
-    prediction = model.predict(data)
-    if prediction[0][0] > 0.3:
-        st.header('Abriendo')
-        client1.publish("IMIA", json.dumps({"gesto": "Abre"}), qos=0, retain=False)
-        time.sleep(0.2)
-    elif prediction[0][1] > 0.3:
-        st.header('Cerrando')
-        client1.publish("IMIA", json.dumps({"gesto": "Cierra"}), qos=0, retain=False)
-        time.sleep(0.2)
+        # Realizar la inferencia
+        prediction = model.predict(data)
+        if prediction[0][0] > 0.3:
+            st.header('Abriendo')
+            client1.publish("IMIA", json.dumps({"gesto": "Abre"}), qos=0, retain=False)
+            time.sleep(0.2)
+        elif prediction[0][1] > 0.3:
+            st.header('Cerrando')
+            client1.publish("IMIA", json.dumps({"gesto": "Cierra"}), qos=0, retain=False)
+            time.sleep(0.2)
+    except Exception as e:
+        st.write(f"Error durante la predicción: {e}")
